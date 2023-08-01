bbdf3982e266d1f81359a1c044c9a83de9d73fb0
thunthean <71697130+TH3an@users.noreply.github.com>
2023-08-01 12:12:24 +0700

    Add whitelist system protection

---
+++ src/main/java/me/thunthean/Commands/TellCMD.java
@@ -0,0 +1,39 @@
+package me.thunthean.Commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Arrays;
+
+import static me.thunthean.main.PREFIX;
+
+public class TellCMD implements CommandExecutor {
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
+        if(sender instanceof Player) {
+            Player player = (Player) sender;
+            if(args.length == 1) {
+                player.sendMessage(PREFIX + "Please do /tell <name> <message>");
+                return true;
+            }
+            if(args.length == 3) {
+                String name = args[0];
+                String message = args[1];
+                Player getname = Bukkit.getPlayer(name);
+
+                if(getname == null || !getname.isOnline()) {
+                    player.sendMessage(PREFIX + "Player is not found");
+                }else{
+                    getname.sendMessage(PREFIX + "[TELL]: " + player.getName() + " -> You: " + Arrays.toString(message.split(" ")));
+                    player.sendMessage(PREFIX + "[TELL]: You -> " + getname.getName() + ": " + Arrays.toString(message.split(" ")));
+                }
+
+            }
+        }
+        return true;
+    }
+}
--- src/main/java/me/thunthean/Events/JoinAndLeave.java
+++ src/main/java/me/thunthean/Events/JoinAndLeave.java
@@ -6,8 +6,12 @@ import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 import org.jetbrains.annotations.NotNull;
 
 import static me.thunthean.main.PREFIX;
@@ -20,10 +24,22 @@ public class JoinAndLeave implements Listener {
        if (main.getInstance().vanish) {
            player.setInvisible(true);
        }
+       //A Layer protection whitelist system
+       //can be spoofed with private client
+       if(!player.isWhitelisted()) {
+           player.sendTitle(ChatColor.RED + "[ALERTS] UNKNOWN PLAYER [ALERTS]","", 1, 20 * 10, 1);
+           player.sendMessage(PREFIX + ChatColor.RED + "Don't spy on us! :)");
+           player.setInvulnerable(true);
+           player.addPotionEffect((new PotionEffect(PotionEffectType.SATURATION, 10000, 1)));
+           player.addPotionEffect((new PotionEffect(PotionEffectType.WEAKNESS, 10000, 1)));
+           player.addPotionEffect((new PotionEffect(PotionEffectType.BLINDNESS, 10000, 1)));
+       }
+
        if(player.isInvisible()) {
            Bukkit.getServer().getConsoleSender().sendMessage(PREFIX + player.getName() + " has join silently (Vanish mode: ON)");
            e.setJoinMessage("");
        }
+
        if(main.getInstance().getConfig().getBoolean("JoinAndLeave")) {
            if (player.hasPlayedBefore()) {
                e.setJoinMessage(ChatColor.BLUE + "[+] " + ChatColor.GRAY + player.getName() + ChatColor.DARK_GRAY + " has joined the server");
@@ -32,12 +48,40 @@ public class JoinAndLeave implements Listener {
            }
        }
    }
+
+   //A Layer protection whitelist system
+   //can be spoofed with private client
+   @EventHandler
+   public void OnMove(PlayerMoveEvent e) {
+       Player player = e.getPlayer();
+       if(!player.isWhitelisted()) {
+           e.setCancelled(true);
+       }
+   }
+    //A Layer protection whitelist system
+    //can be spoofed with private client
+    @EventHandler
+    public void OnCommand(PlayerCommandPreprocessEvent e) {
+       Player player = e.getPlayer();
+       if(!player.isWhitelisted()) {
+           e.setCancelled(true);
+       }
+    }
+
    @EventHandler
     public void OnLeave(@NotNull PlayerQuitEvent e) {
        Player player = e.getPlayer();
        if(player.isInvisible()) {
            Bukkit.getServer().getConsoleSender().sendMessage(PREFIX + player.getName() + " has left silently (Vanish mode: ON)");
            e.setQuitMessage("");
+       }
+       //A Layer protection whitelist system
+       //can be spoofed with private client
+       if(!player.isWhitelisted() && player.hasPotionEffect(PotionEffectType.BLINDNESS) && player.hasPotionEffect(PotionEffectType.WEAKNESS) && player.hasPotionEffect(PotionEffectType.SATURATION)) {
+           player.setInvulnerable(false);
+           player.removePotionEffect(PotionEffectType.BLINDNESS);
+           player.removePotionEffect(PotionEffectType.WEAKNESS);
+           player.removePotionEffect(PotionEffectType.SATURATION);
        }
        if(main.getInstance().getConfig().getBoolean("JoinAndLeave")) {
            e.setQuitMessage(ChatColor.RED + "[-] " + ChatColor.GRAY + player.getName() + ChatColor.DARK_GRAY + " has left the server");
